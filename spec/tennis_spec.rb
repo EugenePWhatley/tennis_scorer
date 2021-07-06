cd78cdd58a6ccb65336c94922c474625dc138666
Eugene Whatley <ewhatley@thoughtworks.com>
2021-07-06 17:32:21 -0400

    Implement logic for 0-0 score

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -1,7 +1,12 @@
+class TennisScorer
+  def score
+    'Love-Love'
+  end
+end
+
 describe TennisScorer do
   it 'can score new game when no player has scored' do
     tennis_scorer = TennisScorer.new
-    score = tennis_scorer.score
-    expect(score).to eq('Love-Love')
+    expect(tennis_scorer.score).to eq('Love-Love')
   end
 end
