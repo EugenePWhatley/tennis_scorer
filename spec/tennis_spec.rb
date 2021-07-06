757685c55bbb544c5230a7531c50448e893235d1
Eugene Whatley <ewhatley@thoughtworks.com>
2021-07-06 17:36:59 -0400

    Increment player 1 score

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -1,6 +1,15 @@
 class TennisScorer
+
+  def initialize
+    @score = "Love"
+  end
+
+  def won_point
+    @score = "Fifteen"
+  end
+
   def score
-    'Love-Love'
+    "#{@score}-Love"
   end
 end
 
@@ -8,5 +17,11 @@ describe TennisScorer do
   it 'can score new game when no player has scored' do
     tennis_scorer = TennisScorer.new
     expect(tennis_scorer.score).to eq('Love-Love')
+  end
+
+  it 'can score game after player one has scored once' do
+    tennis_scorer = TennisScorer.new
+    tennis_scorer.won_point
+    expect(tennis_scorer.score).to eq ('Fifteen-Love')
   end
 end
