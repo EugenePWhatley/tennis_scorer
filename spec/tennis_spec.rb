b720a1895d3e4c0f47efc65a35702300214d25d4
Eugene Whatley <ewhatley@thoughtworks.com>
2021-07-06 18:17:40 -0400

    Move class into it's own file

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -1,29 +1,4 @@
-class TennisScorer
-
-  def initialize
-    @player_one_score = "Love"
-    @player_two_score = "Love"
-  end
-
-  SCORE = {
-    "Love" => "Fifteen",
-    "Fifteen" => "Thirty",
-    "Thirty" => "Forty"
-  }.freeze
-
-  def player_1_scored
-    @player_one_score = SCORE[@player_one_score]
-  end
-
-  def player_2_scored
-    @player_two_score = SCORE[@player_two_score]
-  end
-  
-
-  def score
-    "#{@player_one_score}-#{@player_two_score}"
-  end
-end
+require './tennis'
 
 describe TennisScorer do
   let(:tennis_scorer) { TennisScorer.new } 
